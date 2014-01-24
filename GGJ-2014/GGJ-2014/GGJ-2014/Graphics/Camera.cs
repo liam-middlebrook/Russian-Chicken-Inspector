14a9fe1a23be131aa70c31db1f360a3e3266e011
unknown <afl4947@GDD-64.main.ad.rit.edu>
2014-01-24 18:43:43 -0500

    Added Camera and No Texture

---
+++ GGJ-2014/GGJ-2014/GGJ-2014/Graphics/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace GGJ_2014.Graphics
+{
+    class Camera
+    {
+        private static Matrix cameraMatrix;
+        private static Rectangle screenSize = new Rectangle(0, 0, 800, 500);
+
+        public const float SCALE = 2.0f;
+        
+
+        public static void Focus(float x, float y)
+        {
+            cameraMatrix = Matrix.CreateTranslation(-x + screenSize.Width / 2, -y + screenSize.Height / 2, 1.0f);
+        }
+
+        public static Matrix CameraMatrix
+        {
+            get
+            {
+                return cameraMatrix;
+            }
+        }
+    }
+}
