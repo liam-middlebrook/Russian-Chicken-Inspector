c4af86013eeef7b4e6ebfef374727d6a98455cdc
unknown <afl4947@GDD-64.main.ad.rit.edu>
2014-01-24 18:47:44 -0500

    Working on Level Stuff

---
+++ GGJ-2014/GGJ-2014/GGJ-2014/Level/Level.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GGJ_2014.Level
+{
+    class Level
+    {
+    }
+}
---
+++ GGJ-2014/GGJ-2014/GGJ-2014/Level/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GGJ_2014.Level
+{
+    class Tile
+    {
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
