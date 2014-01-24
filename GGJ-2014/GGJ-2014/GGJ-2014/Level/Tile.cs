2570c502dbedc12557f3941033ea04fb07e743a6
unknown <afl4947@GDD-64.main.ad.rit.edu>
2014-01-24 18:48:10 -0500

    Made sure things did not error on compile

--- GGJ-2014/GGJ-2014/GGJ-2014/Level/Tile.cs
+++ GGJ-2014/GGJ-2014/GGJ-2014/Level/Tile.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace GGJ_2014.Level
 {
     class Tile
     {
+        Rectangle spae;
+
         public void Draw(SpriteBatch spriteBatch)
         {
 
