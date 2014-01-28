fb8c527a09667d6022b7c88488e4a64384193464
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-01-28 15:05:26 -0500

    Fix for clamping penalty values

--- GGJ-2014/GGJ-2014/GGJ-2014/Creatures/GoldenEgg.cs
+++ GGJ-2014/GGJ-2014/GGJ-2014/Creatures/GoldenEgg.cs
@@ -45,10 +45,10 @@ namespace GGJ_2014.Creatures
                     {
                         removeEgg = true;
                         Player.Eggs += (int)Math.Abs(Player.GOLDEN_EGG_VALUE*1.5);
-                        Player.Compassion -= (int)(Math.Min(10 * -Player.Compassion, 20));
+                        Player.Compassion -= (int)(MathHelper.Clamp(10 * -Player.Compassion,-20, 20));
                         MenuSystem.GetInstance().SwitchToMenuScreenOfType(MenuScreenType.GAMEPLAY);
                         MenuSystem.GetInstance().GetMenuScreenOfType(MenuScreenType.PAUSED).menuControls.Clear();
-                        MenuSystem.GetInstance().CurrentScreen.AddControl(new MenuBorderedTextItem(new Vector2(Game1.POPUP_DISPLAY_POSITION_X, Game1.POPUP_DISPLAY_POSITION_Y), Color.Tomato, string.Format("You stole {0} eggs and lost {1} compassion", Player.GOLDEN_EGG_VALUE, (int)Math.Abs((Math.Min(10 * -Player.Compassion, 20)))), 5.0f));
+                        MenuSystem.GetInstance().CurrentScreen.AddControl(new MenuBorderedTextItem(new Vector2(Game1.POPUP_DISPLAY_POSITION_X, Game1.POPUP_DISPLAY_POSITION_Y), Color.Tomato, string.Format("You stole {0} eggs and lost {1} compassion", Player.GOLDEN_EGG_VALUE, (int)(MathHelper.Clamp(10 * -Player.Compassion,-20, 20))), 5.0f));
                     },
                     Microsoft.Xna.Framework.Input.Keys.D1
             ));
